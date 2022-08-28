1ee43260a14b544f482b60c8b0738f05767f8810
Timothy3773 <jonathanscott122@gmail.com>
2022-08-27 20:38:56 -0400

    update

--- src/ServerScriptService/test.server.lua
+++ src/ServerScriptService/test.server.lua
@@ -3,5 +3,5 @@ local test = require(script.Parent.Framework["3D"])
 test:render(game.Workspace, test.defaultSettings)
 
 test.OnCalled:Connect(function(whatisthisfor: nil, ...)
-    print(...)
+    print("Event Received", ...)
 end)
